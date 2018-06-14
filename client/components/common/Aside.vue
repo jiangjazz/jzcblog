4d903c05dd96a1821c6b91dd696d0a36a6ef04c6
Jazz Jiang <526446847@qq.com>
2018-06-15 00:05:44 +0800

    添加细节格式

--- client/components/common/Aside.vue
+++ client/components/common/Aside.vue
@@ -2,7 +2,7 @@
  * @Author: Janzen 
  * @Date: 2018-06-14 22:45:05 
  * @Last Modified by: Janzen
- * @Last Modified time: 2018-06-14 23:34:37
+ * @Last Modified time: 2018-06-14 23:48:03
  */
 <template>
   <div :class="className">
@@ -10,7 +10,7 @@
       <el-menu :collapse="false" :default-openeds="['/blog']" @open="handleOpen" @close="handleClose">
         <nuxt-link to="/blog">
           <el-menu-item index="1">
-            <span slot="title">主页</span>
+            <span slot="title">Object-C简介</span>
           </el-menu-item>
         </nuxt-link>
         <el-submenu index="/blog">
@@ -20,7 +20,7 @@
           <el-menu-item-group title="分组一">
             <nuxt-link to="/blog/basic">
               <el-menu-item index="/blog/basic">
-                课程1
+                基础知识
               </el-menu-item>
             </nuxt-link>
           </el-menu-item-group>
